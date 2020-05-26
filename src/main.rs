5eee5a89c9333a73be6ab84a281cf711a41cc6c9
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2020-05-26 09:34:02 +0900

    Added Simple SDL Layer.  So easy!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::time::Duration;
+use sdl2::event::Event;
+
+fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+    let window = video_subsystem.window("Main Window", 1280, 720)
+        .build().map_err(|x| x.to_string())?;
+    let mut event_pump = sdl_context.event_pump()?;
+
+    'main: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. } => {
+                    println!("Quitting.");
+                    break 'main;
+                }
+                _ => {}
+            }
+        }
+
+        std::thread::sleep(Duration::from_secs_f32(1.0 / 60.0));
+    }
+
+    Ok(())
 }
