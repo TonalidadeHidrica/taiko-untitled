362c4e53f90d4a73c03ad978deac1ea2c33cdc03
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2020-05-26 19:00:41 +0900

    Tried using stainless-ffmpeg, giving up because of flexibility

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,32 @@
 use config::Config;
-use sdl2::event::Event;
-use sdl2::image::LoadTexture;
-use sdl2::pixels::Color;
-use sdl2::rect::Rect;
+use stainless_ffmpeg::format_context::FormatContext;
+use stainless_ffmpeg::stream::Stream;
 use std::path::PathBuf;
-use std::time::Instant;
+use stainless_ffmpeg_sys::AVMediaType::AVMEDIA_TYPE_VIDEO;
+use stainless_ffmpeg::video_decoder::VideoDecoder;
 
 fn main() -> Result<(), String> {
     let mut image_paths = Config::default();
     let image_paths = image_paths
-        .merge(config::File::with_name("image_paths.toml"))
+        .merge(config::File::with_name("config.toml"))
         .map_err(|x| x.to_string())?;
     let image_path = image_paths
-        .get::<PathBuf>("img")
+        .get::<PathBuf>("video")
         .map_err(|x| x.to_string())?;
 
-    let sdl_context = sdl2::init()?;
-    let video_subsystem = sdl_context.video()?;
-    let window = video_subsystem
-        .window("Main Window", 1280, 720)
-        .build()
-        .map_err(|x| x.to_string())?;
-    let mut canvas = window
-        .into_canvas()
-        .present_vsync()
-        .build()
-        .map_err(|x| x.to_string())?;
-    let mut event_pump = sdl_context.event_pump()?;
-
-    let texture_creator = canvas.texture_creator();
-    let texture = texture_creator.load_texture(image_path)?;
-
-    let start_time = Instant::now();
-
-    'main: loop {
-        let elapsed = Instant::now() - start_time;
-        canvas.set_draw_color(Color::WHITE);
-        canvas.clear();
-        canvas.set_draw_color(Color::RED);
-        for i in 0..10 {
-            canvas.copy(
-                &texture,
-                Some(Rect::new(130, 0, 130, 130)),
-                Rect::new(1280 - elapsed.as_millis() as i32 / 10 - i * 30, 100, 130, 130),
-            )?;
+    let mut format_context = FormatContext::new(image_path.to_str().unwrap())?;
+    format_context.open_input()?;
+    let video_stream_idx: isize = (0..format_context.get_nb_streams() as isize)
+        .filter(|x| format_context.get_stream_type(*x) == AVMEDIA_TYPE_VIDEO)
+        .next().ok_or("No video stream found")?;
+    // let video_stream = Stream::new(format_context.get_stream(video_stream_idx));
+    let video_decoder = VideoDecoder::new(String::new(), &format_context, video_stream_idx)?;
+    while let Ok(packet) = format_context.next_packet() {
+        if packet.get_stream_index() != video_stream_idx {
+            continue
         }
-        canvas.present();
-
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. } => {
-                    break 'main;
-                }
-                _ => {}
-            }
-        }
-
-        // std::thread::sleep(Duration::from_secs_f32(1.0 / 60.0));
+        let frame = video_decoder.decode(&packet)?;
+        println!("Frame: pts={}", frame.get_pts());
     }
 
     Ok(())
