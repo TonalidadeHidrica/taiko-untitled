115717bda5d4dcff247c3160592992696092af1e
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2020-05-27 19:12:03 +0900

    Load video with FFmpeg

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,21 @@
-use config::{Config, ConfigError};
+use config::Config;
 use ffmpeg4::format;
 use ffmpeg4::util::{frame, media};
-use std::path::PathBuf;
+use sdl2::event::Event;
 use sdl2::pixels::PixelFormatEnum;
+use std::fmt::Debug;
+use std::path::PathBuf;
 use taiko_untitled::ffmpeg_utils::get_sdl_pix_fmt_and_blendmode;
 
 #[derive(Debug)]
-enum MainErr {
-    ConfigError(ConfigError),
-    StringError(String),
-    FFMpegError(ffmpeg4::Error),
-}
+struct MainErr(String);
 
-impl From<&str> for MainErr {
-    fn from(error: &str) -> Self {
-        MainErr::StringError(error.into())
-    }
-}
-
-impl From<ConfigError> for MainErr {
-    fn from(error: ConfigError) -> Self {
-        MainErr::ConfigError(error)
-    }
-}
-
-impl From<ffmpeg4::Error> for MainErr {
-    fn from(error: ffmpeg4::Error) -> Self {
-        MainErr::FFMpegError(error)
+impl<T> From<T> for MainErr
+where
+    T: ToString,
+{
+    fn from(err: T) -> Self {
+        MainErr(err.to_string())
     }
 }
 
@@ -45,6 +34,23 @@ fn main() -> Result<(), MainErr> {
     let mut decoder = stream.codec().decoder().video()?;
     decoder.set_parameters(stream.parameters())?;
 
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+    let window = video_subsystem
+        .window("Main Window", 1280, 720)
+        .build()
+        .map_err(|x| x.to_string())?;
+    let mut canvas = window
+        .into_canvas()
+        .present_vsync()
+        .build()
+        .map_err(|x| x.to_string())?;
+    let mut event_pump = sdl_context.event_pump()?;
+
+    let texture_creator = canvas.texture_creator();
+    let mut texture =
+        texture_creator.create_texture_streaming(Some(PixelFormatEnum::IYUV), 1280, 720)?;
+
     let mut frame = frame::Video::empty();
     for (_, packet) in input_context
         .packets()
@@ -55,6 +61,32 @@ fn main() -> Result<(), MainErr> {
         }
         let (_, format) = get_sdl_pix_fmt_and_blendmode(frame.format());
         assert!(format == PixelFormatEnum::IYUV && frame.stride(0) > 0);
+        texture.update_yuv(
+            None,
+            frame.data(0),
+            frame.stride(0),
+            frame.data(1),
+            frame.stride(1),
+            frame.data(2),
+            frame.stride(2),
+        )?;
+        break;
+    }
+
+    'main: loop {
+        canvas.copy(&texture, None, None)?;
+        canvas.present();
+
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. } => {
+                    break 'main;
+                }
+                _ => {}
+            }
+        }
+
+        // std::thread::sleep(Duration::from_secs_f32(1.0 / 60.0));
     }
 
     Ok(())
