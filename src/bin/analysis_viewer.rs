60fad47781463ec18e24eeecc5474d1ef629fd3a
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2022-04-26 16:43:27 +0900

    Enable momentum scrolling

--- src/bin/analysis_viewer.rs
+++ src/bin/analysis_viewer.rs
@@ -28,6 +28,31 @@ fn main() -> anyhow::Result<()> {
     let mut config = Config::default();
     let config = config.merge(config::File::with_name("config.toml"))?;
 
+    #[cfg(target_os = "macos")]
+    {
+        use libc::c_void;
+        use objc::{
+            class, msg_send,
+            runtime::{Object, YES},
+            sel, sel_impl,
+        };
+
+        const KEY: &str = "AppleMomentumScrollSupported";
+        let string: *mut Object = unsafe { msg_send![class!(NSString), alloc] };
+        let key: *mut Object = unsafe {
+            msg_send![
+                string,
+                initWithBytes: KEY.as_ptr() as *const c_void
+                length: KEY.len() as u32
+                encoding: 4u32
+            ]
+        };
+        let defaults: *mut Object =
+            unsafe { msg_send![class!(NSUserDefaults), standardUserDefaults] };
+        let _: () = unsafe { msg_send![defaults, setBool: YES forKey: key] };
+        let _: () = unsafe { msg_send![key, release] };
+    }
+
     let data: NotePositionsResult = serde_json::from_reader(File::open(&opts.json_path)?)?;
 
     let width = 1440;
