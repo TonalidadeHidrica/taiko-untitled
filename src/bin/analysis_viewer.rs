cf6b9ad94978ee630f61d67a2985b1bddae0290a
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2022-04-27 09:26:10 +0900

    Extract momentum scroll enabler to a method

--- src/bin/analysis_viewer.rs
+++ src/bin/analysis_viewer.rs
@@ -29,29 +29,7 @@ fn main() -> anyhow::Result<()> {
     let config = config.merge(config::File::with_name("config.toml"))?;
 
     #[cfg(target_os = "macos")]
-    {
-        use libc::c_void;
-        use objc::{
-            class, msg_send,
-            runtime::{Object, YES},
-            sel, sel_impl,
-        };
-
-        const KEY: &str = "AppleMomentumScrollSupported";
-        let string: *mut Object = unsafe { msg_send![class!(NSString), alloc] };
-        let key: *mut Object = unsafe {
-            msg_send![
-                string,
-                initWithBytes: KEY.as_ptr() as *const c_void
-                length: KEY.len() as u32
-                encoding: 4u32
-            ]
-        };
-        let defaults: *mut Object =
-            unsafe { msg_send![class!(NSUserDefaults), standardUserDefaults] };
-        let _: () = unsafe { msg_send![defaults, setBool: YES forKey: key] };
-        let _: () = unsafe { msg_send![key, release] };
-    }
+    enable_momentum_scroll();
 
     let data: NotePositionsResult = serde_json::from_reader(File::open(&opts.json_path)?)?;
 
@@ -259,4 +237,28 @@ fn draw(
 
     canvas.present();
     Ok(())
+}
+
+#[cfg(target_os = "macos")]
+fn enable_momentum_scroll() {
+    use libc::c_void;
+    use objc::{
+        class, msg_send,
+        runtime::{Object, YES},
+        sel, sel_impl,
+    };
+
+    const KEY: &str = "AppleMomentumScrollSupported";
+    let string: *mut Object = unsafe { msg_send![class!(NSString), alloc] };
+    let key: *mut Object = unsafe {
+        msg_send![
+            string,
+            initWithBytes: KEY.as_ptr() as *const c_void
+            length: KEY.len() as u32
+            encoding: 4u32
+        ]
+    };
+    let defaults: *mut Object = unsafe { msg_send![class!(NSUserDefaults), standardUserDefaults] };
+    let _: () = unsafe { msg_send![defaults, setBool: YES forKey: key] };
+    let _: () = unsafe { msg_send![key, release] };
 }
